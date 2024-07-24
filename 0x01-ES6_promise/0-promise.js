04f43f77da666f526024352fdb038844a27d2a9f
recholkibebe <recholkibebe@gmail.com>
2024-07-23 23:30:20 -0700

    new corrections

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,4 @@
-Function getResponseFromAPI(){
-	const promise = Promise.reject(5);
-	return promise;
+/* eslint-disable */
+export default function getResponseFromAPI() {
+	return new Promise((resolve, reject) => {});
 }
-const response = getResponseFromAPI();
-console.log(response instanceof promise);
