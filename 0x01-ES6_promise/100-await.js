8272a02f6b4f8d1283bf4d4f62b372a10dd76eb4
recholkibebe <recholkibebe@gmail.com>
2024-07-24 01:18:13 -0700

    more useruploads

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,19 @@
+import { uploadPhoto, createUser } from './utils';
+
+async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return {
+      photo,
+      user,
+    };
+  } catch (error) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
+
+export default asyncUploadUser;
