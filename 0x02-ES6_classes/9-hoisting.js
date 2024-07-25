4e8fdf34685d1257d179d9c49ba90f3e22f8b06f
recholkibebe <recholkibebe@gmail.com>
2024-07-25 17:35:41 +0300

    correct

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,19 +1,17 @@
-   return this._holbertonClass;
+// HolbertonClass.js
+export class HolbertonClass {
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
   }
 
-  get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+  get year() {
+    return this._year;
+  }
+
+  get location() {
+    return this._location;
   }
 }
 
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
-
-const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
-const student2 = new StudentHolberton('John', 'Doe', class2020);
-const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
-const student4 = new StudentHolberton('Donald', 'Bush', class2019);
-const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
-
-export default [student1, student2, student3, student4, studen5;
 
